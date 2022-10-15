a59431dbe07ce247ab6e03de230719db0bef7d39
Jhonatan <jhonatan.guevara@unmsm.edu.pe>
2022-10-15 16:02:19 -0500

    Prueba

---
+++ api/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+
+/*const routesBodeguero = require("./routesBodeguero");
+const routesCategoria = require("./routesCategoria");
+const routesProducto = require("./routesProducto");
+const routesProveedor = require("./routesProveedor");
+const routesTienda = require("./routesTienda");*/
+
+const app = express();
+app.set("port", process.env.PORT || 5000);
+
+// middlewares -------------------------------------
+app.use(express.json());
+app.use(cors());
+// routes -------------------------------------------
+app.get("/", (req, res) => {
+  res.send("Api funcionando");
+});
+
+/*app.use("/api/bodeguero", routesBodeguero);
+app.use("/api/categoria", routesCategoria);
+app.use("/api/producto", routesProducto);
+app.use("/api/proveedor", routesProveedor);
+app.use("/api/tienda", routesTienda);*/
+
+// server running -----------------------------------
+app.listen(app.get("port"), () => {
+  console.log("server running on port", app.get("port"));
+});
