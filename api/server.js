05b330d327e66e9023c3d94b4fdd1334a63db170
Jhonatan <jhonatan.guevara@unmsm.edu.pe>
2022-10-15 18:31:30 -0500

    v55

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./transaction");
 const routesBD = require("./read");
+const routesOrden = require("./orders");
 
 const app = express();
 app.set("port", process.env.PORT || 5000);
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/transaccion", routes);
 app.use("/api/read", routesBD);
+app.use("/api/ordenes", routesOrden);
 
 // server running -----------------------------------
 app.listen(app.get("port"), () => {
