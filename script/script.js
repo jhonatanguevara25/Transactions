fc65ce132633b09764bbf3bcda0196cf6a76390d
Fiorela Huarcaya <fiorela.16.febrero@gmail.com>
2022-10-15 18:17:47 -0500

    Front - Settings :v

--- script/script.js
+++ script/script.js
@@ -1,3 +1,17 @@
+    fetch(`http://localhost:5000/api/read/account1`, { method: "GET" }).then((response)=> {
+        response.json().then((data) => {
+            const cuenta1 =  document.getElementById("cuenta1");
+            cuenta1.value = data.balance;
+          });
+    })
+
+    fetch(`http://localhost:5000/api/read/account2`, { method: "GET" }).then((response)=> {
+        response.json().then((data) => {
+            const cuenta1 =  document.getElementById("cuenta2");
+            cuenta1.value = data.balance;
+            
+          });
+    })
 
 
 function transferir(id) {
