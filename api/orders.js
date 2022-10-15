afc12d059f71a413eeb342e2711922a29b18a9c5
Jhonatan <jhonatan.guevara@unmsm.edu.pe>
2022-10-15 18:29:31 -0500

    v53

--- api/orders.js
+++ api/orders.js
@@ -11,13 +11,11 @@ routes.get("/:id", (req, res) => {
     const client = new MongoClient(uri);
 
     try {
-      // Connect to the MongoDB cluster
       await client.connect();
 
-      // User1 purchases 1 copy of parks-rec-book
-      await purchaseBook(client, "User1", "leche", 4, "paid");
+      await comprar(client, "User1", "leche", 4, "paid");
+      res.send("Operacion exitosa");
     } finally {
-      // Close the connection to the MongoDB cluster
       await client.close();
     }
   }
@@ -25,8 +23,13 @@ routes.get("/:id", (req, res) => {
   ventasInventario().catch(console.error);
 });
 
-async function transferMoney(client, account1, account2, amount) {
-  const accountsCollection = client.db("banking").collection("accounts");
+async function comprar(client, userId, productID, quantity, status) {
+  const ordersCollection = client.db("yarashop-products").collection("orders");
+
+  const inventoryCollection = client
+    .db("yarashop-products")
+    .collection("inventory");
+
   const session = client.startSession();
 
   const transactionOptions = {
@@ -37,56 +40,51 @@ async function transferMoney(client, account1, account2, amount) {
 
   try {
     const transactionResults = await session.withTransaction(async () => {
-      const subtractMoneyResults = await accountsCollection.updateOne(
-        { _id: account1 },
-        { $inc: { balance: amount * -1 } },
+      const updateInventoryResults = await inventoryCollection.updateOne(
+        { _id: productID },
+        { $inc: { numberInStock: quantity * -1 } },
         { session }
       );
       console.log(
-        `${subtractMoneyResults.matchedCount} document(s) found in the accounts collection with _id ${account1}.`
+        `${updateInventoryResults.matchedCount} document(s) found in the inventory collection with _id ${bookId}.`
       );
       console.log(
-        `${subtractMoneyResults.modifiedCount} document(s) was/were updated to remove the money.`
+        `${updateInventoryResults.modifiedCount} document(s) was/were updated.`
       );
-      if (subtractMoneyResults.modifiedCount !== 1) {
+      if (updateInventoryResults.modifiedCount !== 1) {
         await session.abortTransaction();
         return;
       }
 
-      // Add the money to the second account
-      const addMoneyResults = await accountsCollection.updateOne(
-        { _id: account2 },
-        { $inc: { balance: amount } },
+      const insertOrderResults = await ordersCollection.insertOne(
+        {
+          userId: userId,
+          productID: productID,
+          quantity: quantity,
+          status: status,
+        },
         { session }
       );
       console.log(
-        `${addMoneyResults.matchedCount} document(s) found in the accounts collection with _id ${account2}.`
+        `New order recorded with the following id: ${insertOrderResults.insertedId}`
       );
-      console.log(
-        `${addMoneyResults.modifiedCount} document(s) was/were updated to add the money.`
-      );
-      if (addMoneyResults.modifiedCount !== 1) {
-        await session.abortTransaction();
-        return;
-      }
     }, transactionOptions);
 
     if (transactionResults) {
       console.log(
-        "The money was successfully transferred. Database operations from the transaction are now visible outside the transaction."
+        "The order was successfully processed. Database operations from the transaction are now visible outside the transaction."
       );
     } else {
       console.log(
-        "The money was not transferred. The transaction was intentionally aborted."
+        "The order was not successful. The transaction was intentionally aborted."
       );
     }
   } catch (e) {
     console.log(
-      "The money was not transferred. The transaction was aborted due to an unexpected error: " +
+      "The order was not successful. The transaction was aborted due to an unexpected error: " +
         e
     );
   } finally {
-    // Step 4: End the session
     await session.endSession();
   }
 }
